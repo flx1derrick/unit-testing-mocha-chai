52caa84ca7a9c4fdba817d19f7bd70845b091cfe
Derrick James <95151579+flx1derrick@users.noreply.github.com>
2022-03-23 13:43:41 -0500

    isPerfectSquare test true

--- test/app-test.js
+++ test/app-test.js
@@ -1,7 +1,19 @@
 // Bring in the chai assertion library, mocha, and app.js file
 const {assert} = require('chai');
 const { describe, it } = require('mocha');
+const { isPerfectSquare } = require('../app');
 const app = require('../app');
 
 
 // Write tests function below this line
+describe('-', () => {
+    it('returns true if input is a perfect square', () => {
+      const num = 16;
+      const result = isPerfectSquare(num);
+      const expected = true;
+      
+      
+      // Write assertion here
+      assert.ok(result === expected);
+    });
+  });
